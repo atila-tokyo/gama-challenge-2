ae6e1dae641efdaf8ec93a122f207c2ff73fac90
atila-tokyo <atilamanager@gmail.com>
2021-07-25 16:03:57 +0900

    Adding Home card

--- src/components/Cards/CardHome.js
+++ src/components/Cards/CardHome.js
@@ -1,22 +1,55 @@
 import React from 'react';
 import { Card, CardGroup, Button } from 'react-bootstrap';
+import implementos from '../../assets/implementos.jpeg';
+import sementes from '../../assets/sementes.jpeg'
+import epi from '../../assets/epi.jpeg';
+import consultoria from '../../assets/consultoria.jpeg';
 
 export default function CardHome() {
     return (
-        <CardGroup>
+        <>
+        <CardGroup className="text-center">
+            <Card>
+                <Card.Body>
+                    <Card.Title> Implementos Agrícolas</Card.Title>
+                    <Card.Img style={{ width: '13rem'}} src={implementos}></Card.Img>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title> Sementes</Card.Title>
+                    <Card.Img style={{ width: '13rem'}} src={sementes}></Card.Img>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title> Equipamentos de Proteção</Card.Title>
+                    <Card.Img style={{ width: '13rem'}} src={epi}></Card.Img>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title> Consultoria Técnica</Card.Title>
+                    <Card.Img style={{ width: '13rem'}} src={consultoria}></Card.Img>
+                </Card.Body>
+            </Card>
+
+        </CardGroup>
+        <CardGroup className="text-center">
             <Card>
                 <Card.Body>
                     <Card.Title> Cadastrar Cliente</Card.Title>
-                    <Button></Button>
+                    <Button>Cadastre</Button>
                 </Card.Body>
             </Card>
             <Card>
                 <Card.Body>
                     <Card.Title> Cadastrar Produto</Card.Title>
-                    <Button></Button>
+                    <Button>Cadastre</Button>
                 </Card.Body>
             </Card>
         </CardGroup>
+        </>
     );
 
 }
